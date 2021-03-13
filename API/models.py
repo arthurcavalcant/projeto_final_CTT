6d0d50bce8b69d2945da9bf8453a2b1d8a0bd97e
arthurcavalcant <arthur.ferreira@ccc.ufcg.edu.br>
2021-03-13 10:33:17 -0300

    Add dataclass notation to the models

--- API/models.py
+++ API/models.py
@@ -1,35 +1,69 @@
+from dataclasses import dataclass
+from datetime import date
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
+@dataclass
 class Pessoa(db.Model):
+    id_pessoa: str
+    nome: str
+    cpf: str
+    rg: str
+    data_nascimento: date
+    estado_civil_pessoa: str
+    profissao: str
+
     id_pessoa = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     nome = db.Column(db.VARCHAR(255), nullable=False)
-    cpf = db.Column(db.VARCHAR(11), unique=True)
-    rg = db.Column(db.VARCHAR(7), unique=True)
-    data_nascimento = db.Column(db.DATE, nullable=False)
+    cpf = db.Column(db.CHAR(11), unique=True, nullable=False)
+    rg = db.Column(db.CHAR(7), unique=True, nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
     estado_civil_pessoa = db.Column(db.Enum('solteira', 'casada', 'viúva', 'divorciada', 'união estável',
                                             name='estado_civil'), nullable=False)
     profissao = db.Column(db.VARCHAR(50), nullable=False)
 
 
+@dataclass
 class Proprietario(db.Model):
+    id_proprietario: int
+    id_pessoa: int
+
     id_proprietario = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_pessoa = db.Column(db.INTEGER, db.ForeignKey('pessoa.id_pessoa'), nullable=False)
 
 
+@dataclass
 class Cliente(db.Model):
+    id_cliente: int
+    id_pessoa: int
+
     id_cliente = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
-    id_pessoa = db.Column(db.INTEGER, db.ForeignKey('pessoa.id_pessoa'), nullable=False)
+    id_pessoa = db.Column(db.INTEGER, db.ForeignKey('pessoa.id_pessoa', ondelete='CASCADE'), nullable=False, unique=True)
 
 
 class Vendedor(db.Model):
+    id_vendedor: int
+    id_pessoa: int
+
     id_vendedor = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_pessoa = db.Column(db.INTEGER, db.ForeignKey('pessoa.id_pessoa'), nullable=False)
 
 
 class Imovel(db.Model):
+    id_imovel: int
+    id_proprietario: int
+    tipo_imovel: str
+    rua: str
+    numero: int
+    bloco: str
+    cep: str
+    cidade: str
+    uf: str
+    data_posse_proprietario: date
+
     id_imovel = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_proprietario = db.Column(db.INTEGER, db.ForeignKey('proprietario.id_proprietario'), nullable=False)
     tipo_imovel = db.Column(db.Enum('apartamento', 'casa', 'kitnet', name='tipo_imovel'), nullable=False)
@@ -37,19 +71,31 @@ class Imovel(db.Model):
     numero = db.Column(db.INTEGER, nullable=False)
     andar = db.Column(db.INTEGER, nullable=True)
     bloco = db.Column(db.VARCHAR(25), nullable=True)
-    cep = db.Column(db.INTEGER, nullable=True)
+    cep = db.Column(db.CHAR(8), nullable=True)
     cidade = db.Column(db.VARCHAR(50), nullable=False)
-    uf = db.Column(db.VARCHAR(2), nullable=False)
+    uf = db.Column(db.CHAR(2), nullable=False)
     data_posse_proprietario = db.Column(db.DATE, nullable=False)
 
 
 class Banco(db.Model):
+    id_banco: int
+    codigo_banco: int
+    nome: str
+
     id_banco = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     codigo_banco = db.Column(db.INTEGER, nullable=False)
     nome = db.Column(db.VARCHAR(255), nullable=False)
 
 
 class Compra(db.Model):
+    id_compra: int
+    id_imovel: int
+    id_proprietario: int
+    id_cliente: int
+    id_vendedor: int
+    valor: float
+    tipo_compra: str
+
     id_compra = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_imovel = db.Column(db.INTEGER, db.ForeignKey('imovel.id_imovel'), nullable=False)
     id_proprietario = db.Column(db.INTEGER, db.ForeignKey('proprietario.id_proprietario'), nullable=False)
@@ -60,6 +106,12 @@ class Compra(db.Model):
 
 
 class Financiamento(db.Model):
+    id_financiamento: int
+    id_compra: int
+    id_banco: int
+    parcelas: int
+    entrada_porcentagem: int
+
     id_financiamento = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_compra = db.Column(db.INTEGER, db.ForeignKey('compra.id_compra'), nullable=False)
     id_banco = db.Column(db.INTEGER, db.ForeignKey('banco.id_banco'), nullable=False)
@@ -68,6 +120,11 @@ class Financiamento(db.Model):
 
 
 class GastoPreVenda(db.Model):
+    id_gasto_pre_venda: int
+    id_compra: int
+    nome: str
+    valor: float
+
     id_gasto_pre_venda = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     id_compra = db.Column(db.INTEGER, db.ForeignKey('compra.id_compra'), nullable=False)
     nome = db.Column(db.VARCHAR(255), nullable=False)

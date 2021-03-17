cc306995e8a1ed11f8fa9b4d6c6c5a3aa3f4b6c3
arthurcavalcant <arthur.ferreira@ccc.ufcg.edu.br>
2021-03-17 09:47:46 -0300

    Updates app configuration

--- API/app.py
+++ API/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+
+from API.blueprints import banco
 from models import db
 from configparser import ConfigParser
-from blueprints import cliente, compra, proprietario, vendedor
+from blueprints import cliente, compra, proprietario, vendedor, imovel
 
 app = Flask(__name__)
+
+CORS(app)
 
 config = ConfigParser()
 config.read('settings.ini')
@@ -15,13 +20,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config.get("DATABASE", "
 app.config['SECRET_KEY'] = config.get("APP_KEY", "key")
 
 app.register_blueprint(cliente.cliente_blueprint)
-app.register_blueprint(compra.compra_blueprint)
 app.register_blueprint(proprietario.proprietario_blueprint)
+app.register_blueprint(imovel.imovel_blueprint)
 app.register_blueprint(vendedor.vendedor_blueprint)
+app.register_blueprint(compra.compra_blueprint)
+app.register_blueprint(banco.banco_blueprint)
+
 
 with app.app_context():
     db.init_app(app)
     db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
